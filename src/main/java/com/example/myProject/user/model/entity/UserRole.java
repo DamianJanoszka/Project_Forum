4a23c279716b1b20d884e85bd3d56b653fe445df
d.janoszka <d.janoszka@wasko.pl>
2021-08-17 15:50:14 +0200

    trying to connect with database

---
+++ src/main/java/com/example/myProject/user/model/entity/UserRole.java
@@ -0,0 +1,14 @@
+package com.example.myProject.user.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String role;
+}
